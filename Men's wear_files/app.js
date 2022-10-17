c372fca8481e3f07f2bb0b9222a569e947900d68
groszwi <groszwi@bisnode.com>
2022-10-17 21:47:55 +0300

    adds resources for shop

---
+++ Men's wear_files/app.js
@@ -0,0 +1,12 @@
+$("#burger").click( function() { 
+    $('#menu-content').toggle();
+});
+
+$("#drop-men").click( function() { 
+    $('#men-content').toggle();
+});
+
+$("#drop-women").click( function() { 
+    $('#women-content').toggle();
+});
+
